b50edf7af0bda23b7d0904b2ec7ea1ee2d50a074
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-12 16:04:12 +0100

    feat(types/liquidation): add LiquidationEvent interface

---
+++ src/types/liquidation.ts
@@ -0,0 +1,11 @@
+export interface LiquidationEvent {
+  eventId: number;
+  liquidator: string;
+  borrower: string;
+  /** Debt repaid by the liquidator, in micro-aUSD */
+  debtRepaid: number;
+  /** STX collateral seized by the liquidator, in micro-STX */
+  collateralSeized: number;
+  /** Block height when the liquidation occurred */
+  blockHeight: number;
+}
