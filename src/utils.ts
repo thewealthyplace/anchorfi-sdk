c0f1847a4abf860936227873aefd09c8da5ada73
thewealthyplace <thewealthyplace100@gmail.com>
2026-05-12 16:04:25 +0100

    feat(utils): add micro/STX unit conversions

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+import {
+  INTEREST_PRECISION,
+  INTEREST_RATE_PER_BLOCK,
+  LTV_RATIO,
+  PRICE_PRECISION,
+  RATIO_PRECISION,
+} from './constants';
+
+/** Convert micro-STX to STX (1 STX = 1_000_000 micro-STX). */
+export function microToStx(micro: number | bigint): number {
+  return Number(micro) / 1_000_000;
+}
+
+/** Convert STX to micro-STX. */
+export function stxToMicro(stx: number): bigint {
+  return BigInt(Math.round(stx * 1_000_000));
+}
